36fa5104abfcbe3ecc6dc8b20636d24ac7b84973
Davis O <davisokoth@gmail.com>
2019-04-01 18:47:24 +0300

    project: prefer `tailwindcss`
    Add tailwindcss for styling

--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -1,13 +1,16 @@
 import { TestBed, inject } from '@angular/core/testing';
-import { HttpClientModule} from '@angular/common/http';
+// import { HttpClientModule} from '@angular/common/http';
+// import { HttpClientTestingModule } from '@angular/httpClient';
 import { UserService } from './user.service';
+import { of } from 'rxjs'; // Add import
+import { GitResult } from '../models/git-result.model';
 
-describe('UserService', () => {
+describe('UsersService', () => {
 
   let subject: UserService;
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule],
+      // imports: [HttpClientModule],
       providers: [UserService]
     }).compileComponents();
   });
@@ -18,5 +21,32 @@ describe('UserService', () => {
 
   it('should be created', () => {
     expect(subject).toBeTruthy();
+  });
+
+  it('should return a collection of users', () => {
+    const userResponse: GitResult = {
+      total_count: 2,
+      incomplete_results: false,
+      items: [
+        {
+          login: 'janedoe@jd.com',
+          email: 'janedoe@jd.com',
+          name: 'Jane'
+        },
+        {
+          login: 'janedoe@jd.com',
+          email: 'janedoe@jd.com',
+          name: 'Jane'
+        }
+      ]
+    };
+    let response;
+    spyOn(subject, 'getGitUsers').and.returnValue(of(userResponse));
+
+    subject.getGitUsers(`query`, 0).subscribe(res => {
+      response = res;
+    });
+
+    expect(response).toEqual(userResponse);
   });
 });
