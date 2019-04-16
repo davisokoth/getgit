0cd6eaa3a3968f8f699b9a3594483aaf50805768
Davis O <davisokoth@gmail.com>
2019-04-16 11:21:54 +0300

    project: add build script

---
+++ src/scripts/build.sh
@@ -0,0 +1,12 @@
+echo "Beginning build..."
+echo "==================================="
+echo ""
+ng build --prod --base-href / --build-optimizer --output-hashing=all
+echo ""
+echo "Now deploying..."
+echo ""
+rm -r /var/www/html/*
+cp -r dist/* /var/www/html/
+echo ""
+echo "====================================="
+echo "End build "
